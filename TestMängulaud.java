6707221e0c62564790de166707534a0b113a30f3
hvirro <34108918+hvirro@users.noreply.github.com>
2018-03-13 23:46:53 +0200

    Update TestMängulaud.java

--- TestMängulaud.java
+++ TestMängulaud.java
@@ -1,8 +1,22 @@
 public class TestMängulaud {
+    public static char tähis() {
+        char[] tähised = {'X', 'O'};
+        int indeks = (int)Math.round((Math.random()*1));
+        char mängija = tähised[indeks];
+        return mängija;
+    }
+    
     public static void main(String[] args) {
         Mängulaud laud = new Mängulaud();
-        char mängija1 = 'X';
-        char mängija2 = 'O';
+        // Tähise määramine
+        char mängija1 = tähis();
+        char mängija2;
+        if (mängija1 == 'X') {
+            mängija2 = 'O';
+        }
+        else {
+            mängija2 = 'X';
+        }
 
         laud.tühiLaud();
         laud.hetkeseis();
